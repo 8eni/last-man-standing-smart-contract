52d7820012713d686ac9e6e34b519f5424af5b1b
Benni Russell <bennirus@Sarahs-MacBook-Pro.local>
2017-06-20 17:54:38 +0100

    adds updated contract end of day2

--- app/metacoin/metacoin.component.js
+++ app/metacoin/metacoin.component.js
@@ -15,32 +15,82 @@ let metacoinComponent = {
 		var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 		// Contract details
 		var contractAbi = CreditStateControllerContract.abi;
-		var contractAddress = '0x4ac003c2a627766299a6ee93bea2b06373950885'; // CreditStateControllerContract.networks[0].address
+		var contractAddress = '0x43b06360cfb31d3a74361319037d686d8ada62f7'; // CreditStateControllerContract.networks[0].address
 		var contract = web3.eth.contract(contractAbi).at(contractAddress); // Same as MetaCoin.deployed() in truffle console
 			
+			// console.log('contract',contract)
+		vm.days = ['1 day', '2 days', '3 days']
+		vm.selectedDay = vm.days[0];
+		function getSeconds(arg) {
+			if (arg == vm.days[0]) {
+				return 86400
+			} else if (arg == vm.days[1]) {
+				return 172800
+			} else {
+				return 259200
+			}
+		}
+
+		// Retrieve last transaction state
+		function getCurrentState() {
+			var blockDetails = web3.eth.getBlock(web3.eth.blockNumber)
+			var txnDetail = web3.eth.getTransactionReceipt(blockDetails.transactions[0])
+			if (web3.toDecimal(txnDetail.logs[2].data) == 1) {
+				vm.creditReportState = 'true';
+			} else {
+				vm.creditReportState = 'false';
+			}
+		}
+		// getCurrentState();
+		console.log('vm.creditReportState',vm.creditReportState)
 		vm.addresses = contract._eth.accounts;
 		vm.addressOne = vm.addresses[0];
 		vm.addressTwo = vm.addresses[1];
 		vm.selectedAddress = vm.addresses[0];
-		// vm.balanceInEth = contract.getBalanceInEth.call(vm.addressOne).toString(10)	
-		vm.balance = 0;
-		vm.creditReportState = false; 
-		// console.log('TXNs',web3.eth.getTransaction('0xd84b3d913c4e6297a9c552c11b5cd39a7d3c3eee3e2e3ba6454866908c21fe98'))
-		// console.log('TXNs',web3.eth.getTransaction())
-		
-		function getTransactionReceipt(arg) {
-			// var logs = [];
-			var receipt = web3.eth.getTransactionReceipt(arg);
-			// for (var i = receipt.logs.length - 1; i >= 0; i--) {
-			// 	logs.push(web3.toAscii(receipt.logs[i].data))
-			// }
-			return web3.toAscii(receipt.logs[0].data)
 
-			// console.log('logs',web3.toAscii(receipt.logs[0].data))
+		vm.duration = 86400;
+		
+		function getTransactionReceipt(arg) { // TXN hash passed as param
+			// console.log('TXN',arg)
+			var logs = [];
+			var receipt = web3.eth.getTransactionReceipt(arg);
+			// console.log('receipt',receipt)
+			for (var i = 0; i < receipt.logs.length; i++) {
+				// console.log(receipt.logs.length)
+				if (i == 0) { // Address
+					logs.push(web3.toAscii(receipt.logs[i].data))
+				} else if (i == 1) { // Duration
+					logs.push(web3.toDecimal(receipt.logs[i].data))
+				} else if (i == 2) { // Duration
+					(web3.toDecimal(receipt.logs[i].data) == 1) ? logs.push(true) : logs.push(false) 
+				} else if (i == 3) { // Duration
+					logs.push(secondsConverter(web3.toDecimal(receipt.logs[i].data)))
+				} else if (i == 4) { // Duration
+					logs.push(secondsToHms(web3.toDecimal(receipt.logs[i].data)))
+				} else {
+					logs.push(web3.toDecimal(receipt.logs[i].data))
+				}
+
+			}
+			console.log('logs',logs)
+			return logs
+		}
+		function secondsToHms(d) {
+		    d = Number(d);
+		    var h = Math.floor(d / 3600);
+		    var hDisplay = h > 0 ? h + (h == 1 ? " hr" : " hrs") : "";
+		    return hDisplay; 
+		}
+		function secondsConverter(time) {
+			var date = new Date(null);
+			date.setSeconds(time); // specify value for SECONDS here
+			return date.toISOString().substr(11, 8);
 		}
 
+
 		vm.transactions = [];
-		function pushTxn(obj, bool) {
+		// my_array[my_array.length - 1]
+		function pushTxn(obj) {
 			var txnObject;
 			var txnHash;
 			if (obj.hash) {
@@ -51,29 +101,42 @@ let metacoinComponent = {
 				txnObject = web3.eth.getTransaction(obj);
 				getTransactionReceipt(obj)
 			}
+			// console.log('txnObject',txnObject)
+			var txnAdd = getTransactionReceipt(txnHash);
 			var txn = {
 				'hash': txnObject.hash,
 				'blockNumber': txnObject.blockNumber,
 				'from': txnObject.from,
-				'state': bool,
-				'gas': txnObject.gas,
-				'log': getTransactionReceipt(txnHash)
+				'address': 'NA', //txnAdd[1]
+				'bool': txnAdd[2],
+				'timestamp': txnAdd[3],
+				'duration': txnAdd[4],
+				'message': txnAdd[0]
 			}
 			vm.transactions.push(txn);
+			// console.log('vm.transactions',vm.transactions)
+
+
 		}
+
+		// SWITCH freeze 1 param unfreeze 2 params next iteration of contract
 		vm.freeze = () => {
-			contract.freeze(vm.addressOne, {from: vm.selectedAddress}, (err, res) => {
+			contract.freeze(vm.addressOne, {from: vm.selectedAddress, gas: 100000}, (err, res) => {
+
 			  if (!err)
-			    pushTxn(res, true)
-				vm.creditReportState = contract.queryCurrentState.call(vm.addressOne).toString(10);
+			    pushTxn(res)
+				// vm.creditReportState = contract.queryCurrentState.call(vm.addressOne).toString(10);
+				vm.creditReportState = 'true'
 				$scope.$apply()
 			});
 		}
 		vm.unFreeze = () => {
-			contract.unFreeze(vm.addressOne, {from: vm.selectedAddress}, (err, res) => {
+			contract.unFreeze(vm.addressOne, "AIB", getSeconds(vm.selectedDay), "Unknown reason", {from: vm.selectedAddress, gas: 400000}, (err, res) => {
+			  console.log('err', err)
 			  if (!err)
-			    pushTxn(res, false)
-				vm.creditReportState = contract.queryCurrentState.call(vm.addressOne).toString(10);
+			    pushTxn(res)
+				// vm.creditReportState = contract.queryCurrentState.call(vm.addressOne).toString(10);
+				vm.creditReportState = 'false';
 				$scope.$apply()
 			});
 		}
@@ -98,6 +161,8 @@ let metacoinComponent = {
 		}
 		var firstPage = web3.eth.blockNumber - 10;
 		getTransactionsByAccount(vm.addressOne, firstPage, web3.eth.blockNumber);
+
+
   
 	}
 
--- contracts/CreditStateController.sol
+++ contracts/CreditStateController.sol
@@ -3,37 +3,114 @@ pragma solidity 0.4.11;
 contract CreditStateController {
  
   address public contractOwner;
-  mapping (address => bool) creditStateMap;
+  mapping (address => creditFreezeTimeLimit) creditStateMap;
  
-  event LogCreditState(address initiator, bool isFrozen);
+  struct creditFreezeTimeLimit {
+                              string[] electedCreditors;
+                              uint[] unfreezeTimings;
+                              uint[] unfreezeDuration;
+                              string[] reasonForUnfreeze;
+  }
+ 
+  
+  event LogUserAddress(address initiator);
+  event LogCreditState(bool isFrozen);
+  event LogCreditFreezeTime(uint freezeTime);
+  event LogCreditFreezeDuration(uint freezeDuration);
   event LogEvent(string description);
+  event logElectedCreditor(string electedCreditor);
+  event logReasonForUnfreeze(string reason);
  
   function CreditStateController(){
     LogEvent("Invoking the contract");
     contractOwner = msg.sender;
   }
  
-  function freeze(address initiator){
-    LogEvent("Freezing the contract for the initiator"); 
-    creditStateMap[initiator] = true;
-    LogCreditState(initiator, creditStateMap[initiator]);
-  }
- 
-  function unFreeze(address initiator){
+  function unFreeze(address initiator, string creditor, uint freezeLimitTime,
+                        string reason) {
+                           
     LogEvent("Un-freezing the contract for the initiator");
-    creditStateMap[initiator] = false;
-    LogCreditState(initiator, creditStateMap[initiator]);
+    recordCreditUnFreezeAndLogEvent(initiator, creditor, freezeLimitTime, reason);
   }
  
-  function queryCurrentState(address initiator) constant returns (bool){
-    return creditStateMap[initiator];
+  function unFreeze(address initiator, string creditor, string reason) {
+    LogEvent("Un-freezing the contract for the initiator");
+    recordCreditUnFreezeAndLogEvent(initiator, creditor, 172800, reason);
   }
  
-  function killContract()  {
-    if(msg.sender != contractOwner) throw;
-    LogEvent("Killing the contract");
-    selfdestruct(contractOwner);
+  function freeze(address initiator){
+               LogEvent("Freezing the contract for the initiator");
+               recordCreditFreezeAndLogEvent(initiator);
   }
  
   
+  
+  function recordCreditUnFreezeAndLogEvent(address initiator, string creditor,
+                    uint freezeLimitTime, string reason) private {
+                       
+    bool creditorInUnfreeze =  false;
+    uint creditorIndex = 0;
+   
+    if(creditStateMap[initiator].electedCreditors.length > 0) {
+               for(uint i=0; i < creditStateMap[initiator].electedCreditors.length; i++ ) {
+                   if(equal(creditStateMap[initiator].electedCreditors[i],creditor)) {
+                       creditorInUnfreeze = true;
+                       creditorIndex = i;
+                       break;
+                   }
+               }
+    }
+              
+               creditStateMap[initiator].electedCreditors.push(creditor);
+    creditStateMap[initiator].unfreezeTimings.push(now);
+    creditStateMap[initiator].unfreezeDuration.push(freezeLimitTime);
+    creditStateMap[initiator].reasonForUnfreeze.push(reason);
+    
+               LogUserAddress(initiator);
+               logElectedCreditor(creditStateMap[initiator].electedCreditors[creditorIndex]);
+               LogCreditState(false);
+               LogCreditFreezeTime(creditStateMap[initiator].unfreezeTimings[creditorIndex]);
+               LogCreditFreezeDuration(creditStateMap[initiator].unfreezeDuration[creditorIndex]);
+               logReasonForUnfreeze(creditStateMap[initiator].reasonForUnfreeze[creditorIndex]);
+ 
+    }
+ 
+   function recordCreditFreezeAndLogEvent(address initiator) private {
+    for(uint j=0; j < creditStateMap[initiator].electedCreditors.length ; j++){
+        delete creditStateMap[initiator];
+    }
+    LogUserAddress(initiator);
+  }
+  
+  function killContract()  {
+    if(msg.sender != contractOwner) throw;
+    LogEvent("Killing the contract");
+   
+    selfdestruct(contractOwner);
+  }
+ 
+  function compare(string _a, string _b) returns (int) {
+        bytes memory a = bytes(_a);
+        bytes memory b = bytes(_b);
+        uint minLength = a.length;
+        if (b.length < minLength) minLength = b.length;
+        //@todo unroll the loop into increments of 32 and do full 32 byte comparisons
+        for (uint i = 0; i < minLength; i ++)
+            if (a[i] < b[i])
+                return -1;
+            else if (a[i] > b[i])
+                return 1;
+        if (a.length < b.length)
+            return -1;
+        else if (a.length > b.length)
+            return 1;
+        else
+            return 0;
+    }
+   
+    /// @dev Compares two strings and returns true iff they are equal.
+  function equal(string _a, string _b) returns (bool) {
+        return compare(_a, _b) == 0;
+  }
+ 
 }
